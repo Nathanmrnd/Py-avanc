692a7c49fcca62c3c49d21285b7fc5c8d88d321a
Nathan Mornand <nathanlouis.mornand@gmail.com>
2024-11-05 16:53:45 +0100

    Modif

--- snake/Serpent.py
+++ snake/Serpent.py
@@ -1,2 +1,11 @@
+import argparse
+
+
 def serpent():
     print("J'ai peur des serpents !")
+
+
+
+parser = argparse.ArgumentParser(description='Some description.')
+parser.add_argument('-a', help="A text explaining what is the use of the -a option and what type of value it takes.")
+args = parser.parse_args()
