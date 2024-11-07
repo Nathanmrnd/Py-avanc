ba7fd8a873e3bae6d85a07fda1dbf1694cd87f88
Nathan Mornand <nathanlouis.mornand@gmail.com>
2024-11-07 23:38:34 +0100

    le serpent est bueno

--- snake/jeu.py
+++ snake/jeu.py
@@ -5,7 +5,8 @@ import argparse
 MIN_SIZE = 200
 W_DEFAULT =  500
 H_DEFAULT = 500
-nathan =0
+
+
 
 def argue():
 
@@ -13,15 +14,15 @@ def argue():
     parser.add_argument('-L', type=int, help="Width")
     parser.add_argument('-l', type=int, help="Heights")
     args = parser.parse_args()
-    return args
+    
 
     # Check argument
-    #if args.s < MIN_SIZE:
-     #   raise ValueError("The size (-s argument) must be greater or equal to %d." % MIN_SIZE)
+    if min(args.L,args.l) < MIN_SIZE:
+        raise ValueError("The size (-s argument) must be greater or equal to %d." % MIN_SIZE)
+    
+    return args
 
-def echiquier(w,h,n): #n**2:nombre de case 
-    screen = pygame.display.set_mode( (w, h) )
-    pygame.display.set_caption("Snape")
+def echiquier(screen,w,h,n): #n**2:nombre de case 
     screen.fill( (0, 255, 0) )
     black = (0, 0, 0)   
     white = (255,255,255)
@@ -36,19 +37,29 @@ def echiquier(w,h,n): #n**2:nombre de case
             pygame.display.update()
 
 
-def snake(t,locx,locy,n,w,h):
+
+
+def snake(screen,Localisation,n,w,h): #localisation : liste contenant les coordonées du coin haut gauche chaque rectangle du snake
     green=(0,255,0)
-    rect=pygame.Rect(locx,locy,w*3/(5*n), h*3/(5*n))
+    for (locx,locy) in Localisation:
+        rect=pygame.Rect(locx,locy,w*3/(5*n), h*3/(5*n))
+        pygame.draw.rect(screen, green, rect)
+        pygame.display.update()
 
 
 
 
 
-def screen():
-    (w,h)=(W_DEFAULT,H_DEFAULT)
+def game():
+    args=argue()
+    (w,h)=(args.L,args.l)
+    n=16 #nombre de ligne/colonne
+    #o initialise la localisation du snake
+    localisation=[(w/5 + (n//2 - 2)*w*3/(5*n), h/5+ (n//2)*h*3/(5*n)),(w/5 + (n//2 -1)*w*3/(5*n), h/5+ (n//2)*h*3/(5*n)),(w/5 + (n//2)*w*3/(5*n), h/5+ (n//2)*h*3/(5*n))]
     pygame.init() 
-    n=15
-    echiquier(w,h,n)
+    screen = pygame.display.set_mode( (w, h) )
+    pygame.display.set_caption("Snape")
+    
 
     clock = pygame.time.Clock()
 
@@ -56,9 +67,11 @@ def screen():
     while True:
         # Wait one second, starting from last display or now
         clock.tick(1)
+        echiquier(screen,w,h,n)
+        snake(screen, localisation,n,w,h)
         # Process new events (keyboard, mouse)
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN or event.type == pygame.QUIT:
                 if event.key == pygame.K_q:
                     return True
 
