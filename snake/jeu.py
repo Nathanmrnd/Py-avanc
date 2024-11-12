e3cc9ff471877a034f5892936f8990d5b66736ae
Nathan Mornand <nathanlouis.mornand@gmail.com>
2024-11-12 15:06:49 +0100

    Déplacement serpent (sans classe!)

--- snake/jeu.py
+++ snake/jeu.py
@@ -23,9 +23,10 @@ def argue(): #permet à l'utilsateur de choisir la hauteur (heights) et la large
     return args
 
 def echiquier(screen,w,h,n): #n**2:nombre de case 
-    screen.fill( (0, 255, 0) )
     black = (0, 0, 0)   
     white = (255,255,255)
+    screen.fill(black)
+    
     for i in range(n):
         for j in range(n):
             rect = pygame.Rect(w/5 + (i)*w*3/(5*n), h/5+ (j)*h*3/(5*n), w*3/(5*n), h*3/(5*n))
@@ -47,7 +48,33 @@ def snake(screen,Localisation,n,w,h): #localisation : liste contenant les coordo
         pygame.display.update()
 
 
+def déplacement_droite(localisation,w,h,n):
+    localisation.remove(localisation[0])
+    (locx,locy)=localisation[-1]
+    (locx,locy)=(locx+w*3/(5*n),locy)
+    localisation.append((locx,locy))
+    return localisation
 
+def déplacement_bas(localisation,w,h,n):
+    localisation.remove(localisation[0])
+    (locx,locy)=localisation[-1]
+    (locx,locy)=(locx,locy+h*3/(5*n))
+    localisation.append((locx,locy))
+    return localisation
+
+def déplacement_haut(localisation,w,h,n):
+    localisation.remove(localisation[0])
+    (locx,locy)=localisation[-1]
+    (locx,locy)=(locx,locy-h*3/(5*n))
+    localisation.append((locx,locy))
+    return localisation
+
+def déplacement_gauche(localisation,w,h,n):
+    localisation.remove(localisation[0])
+    (locx,locy)=localisation[-1]
+    (locx,locy)=(locx-w*3/(5*n),locy)
+    localisation.append((locx,locy))
+    return localisation
 
 
 def game():
@@ -56,6 +83,7 @@ def game():
     n=16 #nombre de ligne/colonne
     #on initialise la localisation du snake
     localisation=[(w/5 + (n//2 - 2)*w*3/(5*n), h/5+ (n//2)*h*3/(5*n)),(w/5 + (n//2 -1)*w*3/(5*n), h/5+ (n//2)*h*3/(5*n)),(w/5 + (n//2)*w*3/(5*n), h/5+ (n//2)*h*3/(5*n))]
+    direction = 'Droite'
     pygame.init() 
     screen = pygame.display.set_mode( (w, h) )
     pygame.display.set_caption("Snape")
@@ -65,17 +93,38 @@ def game():
 
 
     while True:
+        
         # Wait one second, starting from last display or now
         clock.tick(1)
-        echiquier(screen,w,h,n) #il faut à chaque fois redessiner l'échiquier pour voir le déplacement du serpent
-        snake(screen, localisation,n,w,h) #on redessine le serpent à sa nouvelle position à chaque fois
         # Process new events (keyboard, mouse)
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN or event.type == pygame.QUIT:
+            if event.type == pygame.QUIT :
+                return True
+            if event.type == pygame.KEYDOWN :
                 if event.key == pygame.K_q:
                     return True
+                if event.key == pygame.K_DOWN :
+                    direction = 'Bas'
+                if event.key == pygame.K_LEFT :
+                    direction = 'Gauche'
+                if event.key == pygame.K_UP :
+                    direction = 'Haut'
+                if event.key == pygame.K_RIGHT :
+                    direction = 'Droite'
+        
 
-
+        if direction == 'Haut':
+            localisation = déplacement_haut(localisation,w,h,n)
+        if direction == 'Bas':
+            localisation = déplacement_bas(localisation,w,h,n)
+        if direction == 'Droite':
+            localisation = déplacement_droite(localisation,w,h,n)
+        if direction == 'Gauche':
+            localisation = déplacement_gauche(localisation,w,h,n)
+            
+        echiquier(screen,w,h,n) #il faut à chaque fois redessiner l'échiquier pour voir le déplacement du serpent
+        snake(screen, localisation,n,w,h) #on redessine le serpent à sa nouvelle position à chaque fois
+        
 
     pygame.quit()
     return args
